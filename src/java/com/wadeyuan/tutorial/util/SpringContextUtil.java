48eb896c75896a4ae744a90b584d71710a059b40
wadewyuan <wadeyuanwy@hotmail.com>
2016-11-26 19:06:36 +0800

    Debugged & reviewed the code to initialize beans, adding comments

--- src/java/com/wadeyuan/tutorial/util/SpringContextUtil.java
+++ src/java/com/wadeyuan/tutorial/util/SpringContextUtil.java
@@ -11,6 +11,13 @@ public class SpringContextUtil implements ApplicationContextAware{
 
     private static ApplicationContext applicationContext = null;
 
+    /**
+     * Invoked in ApplicationContextAwareProcessor, one of BeanPostProcessors which do their job after bean initialized.
+     * So this class has to be configured in bean factory
+     *
+     * @param applicationContext
+     * @throws BeansException
+     */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("Setting application context:" + SpringContextUtil.class.getName());
