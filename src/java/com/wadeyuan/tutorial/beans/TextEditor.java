483bb9de955a1165b5b5f532c851ce45bc900921
wadewyuan <wadeyuanwy@hotmail.com>
2016-11-27 19:41:22 +0800

    Annotation-based DI

--- src/java/com/wadeyuan/tutorial/beans/TextEditor.java
+++ src/java/com/wadeyuan/tutorial/beans/TextEditor.java
@@ -1,11 +1,15 @@
 package com.wadeyuan.tutorial.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by wadey on 11/27/2016.
  */
 public class TextEditor {
 
     private SpellChecker spellChecker;
+    @Autowired
+    private Configuration configuration;
 
     public TextEditor() {
         System.out.println("Initializing: " + this.toString());
@@ -13,17 +17,30 @@ public class TextEditor {
 
     public void setSpellChecker(SpellChecker spellChecker) {
         this.spellChecker = spellChecker;
-        System.out.println("Injecting spellChecker for TextEditor...");
+        System.out.println("Injecting spellChecker: " + spellChecker);
+    }
+
+    /**
+     * Autowire will not call the setter method to inject dependency, TODO: Review & debug the code
+     * @param configuration
+     */
+    public void setConfiguration(Configuration configuration) {
+        System.out.println("Injecting configuration: " + configuration);
+        this.configuration = configuration;
     }
 
     public void input(String input) {
-        spellChecker.checkSpelling(input);
+        System.out.println("Input of editor: " + input);
+        if(configuration.isSpellingCheckEnabled()) {
+            spellChecker.checkSpelling(input);
+        }
     }
 
     @Override
     public String toString() {
         return "TextEditor{" +
                 "spellChecker=" + spellChecker +
+                ", configuration=" + configuration +
                 '}';
     }
 }
